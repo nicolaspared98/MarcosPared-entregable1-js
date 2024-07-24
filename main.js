f472b2a1ae31c5a7a2f818aaf160f9f03e6d1413
Nicolas Pared <marcosnicolaspared@outlook.com>
2024-07-24 20:50:12 -0300

    Inicio de proyecto js

---
+++ main.js
@@ -0,0 +1,6 @@
+let bandera = true
+let productos = ["Manzana", "Naranja", "Banana", "Uva"]
+ 
+while(bandera){
+    const valorElegido =prompt("Que deseas comprar? \n\n-" + productos.join("\n-"))
+}
